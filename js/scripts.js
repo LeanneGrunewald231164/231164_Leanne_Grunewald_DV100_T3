b097cffdf940bd1697509638f288b0da6d1586e2
LeanneGrunewald231164 <231164@virtualwindow.co.za>
2024-05-22 17:36:40 +0200

    Flights page: Added hover effects; Contact page: Added hover effects; Home+Contact+Flights pages: Added link to About page; About page: Added links to other pages; JavaScript: Added comments for modal  and missing <p> tags

--- js/scripts.js
+++ js/scripts.js
@@ -1,90 +1,121 @@
 /** FLIGHTS **/
 
-let flightsInCart = new Array();
+/** STILL IN PROGRESS **/
 
-// TEST DATA FOR DISPLAY PURPOSES
+// Array containing the flights
+let flightsInCart = [];
+
+// TEST DATA FOR DISPLAY PURPOSES AND TEST PURPOSES
+// Each array entry contains an object containing the details of the flight added
 flightsInCart.push({flightName: "Moonlight Meander", flightAmount: 1, flightPrice: "15000"});
 flightsInCart.push({flightName: "Uranus Uncharted", flightAmount: 2, flightPrice: "50000"});
 
 let flightTotal = 0;
 
 
-// When pressing on the button and not yet in the array, add to the array, else add in array
-// if 0 = remove from array, or minus
+// ToDo: When pressing the "Book now" button, check if flight is in cart, if it is, increment the amount with the amount on the button (next to Book now). If not, add to array.
+// ToDo: Change the button from "Book now" to "Remove" once flight has been added to the cart. Remove flight from basket and change button back when user press the "Remove" button
 
-const cartModal = document.getElementById('cartModal')
+// Get the Modal by id
+const cartModal = document.getElementById('cartModal');
+// Does the modal element exist? 
 if (cartModal) {
-  cartModal.addEventListener('show.bs.modal', function (event) {
-
-    let cartModalBodyContent = `
-      
-      <div class="container-fluid">
-      <div class="row"><div class="col">Name of Flight:</div>
-      <div class="col">Amount of tickets:</div>
-      <div class="col">Price per ticket:</div>
-      <div class="col">Total of flight:</div></div>
-
-    `;
   
-    for (let i = 0; i < flightsInCart.length; i++) {
-      console.log(flightsInCart[i])
+  // When user presses on the "View Cart" button, call the getModalValues function
+  cartModal.addEventListener('show.bs.modal', getModalValues);
 
-      flightTotal += flightsInCart[i].flightAmount * flightsInCart[i].flightPrice;
+}
 
-      cartModalBodyContent += `
+function getModalValues(){
+
+  // Content to be displayed in modal
+  // Uses Bootstrap grid for display, but can be changed to tables
+  // Heading "row"
+  let cartModalBodyContent = `
     
-        <div class="row"><div class="col">${flightsInCart[i].flightName}</div>
-        <div class="col">${flightsInCart[i].flightAmount}</div>
-        <div class="col">${flightsInCart[i].flightPrice}</div>
-        <div class="col">${flightsInCart[i].flightAmount * flightsInCart[i].flightPrice}</div></div>
-  
-      `;
-    }
+    <div class="container-fluid">
+    <div class="row"><div class="col"><p>Name of Flight:</p></div>
+    <div class="col"><p>Amount of tickets:</p></div>
+    <div class="col"><p>Price per ticket:</p></div>
+    <div class="col"><p>Total of flight:</p></div></div>
 
+  `;
+
+  // For each flight in the array
+  for (let i = 0; i < flightsInCart.length; i++) {
+
+    // Testing
+    console.log(flightsInCart[i])
+
+    // Total price per flight = Amount of tickets per flight * Price per ticket
+    flightTotal += flightsInCart[i].flightAmount * flightsInCart[i].flightPrice;
+
+    // Content to be displayed, as per the array
+    // As mentioned, uses Bootstrap grid for display, but can be changed to tables
     cartModalBodyContent += `
-    
-      <div class="row"><div class="col"><p>Final Total:</p></div>
-      <div class="col"></div>
-      <div class="col"></div>
-      <div class="col">${flightTotal}</div></div>
   
-    `;
-
-    cartModalBodyContent += `
-    
-      </div>
+      <div class="row"><div class="col"><p>${flightsInCart[i].flightName}</p></div>
+      <div class="col"><p>${flightsInCart[i].flightAmount}</p></div>
+      <div class="col"><p>R${flightsInCart[i].flightPrice}</p></div>
+      <div class="col"><p>R${flightsInCart[i].flightAmount * flightsInCart[i].flightPrice}</p></div></div>
 
     `;
+  }
 
-    // Update the modal's content.
-    let cartModalBody = cartModal.querySelector('.modal-body .modalBodyFlights');
+  // Final "row" with total
+  cartModalBodyContent += `
   
-    cartModalBody.innerHTML = cartModalBodyContent;
+    <div class="row"><div class="col"><p>Final Total:</p></div>
+    <div class="col"></div>
+    <div class="col"></div>
+    <div class="col"><p>R${flightTotal}</p></div></div>
+
+  `;
+
+  // Final div to close container
+  cartModalBodyContent += `
   
-  })
+    </div>
+
+  `;
+
+  // Using querySelector for classes
+  let cartModalBody = cartModal.querySelector('.modal-body .modalBodyFlights');
+
+  // Placed html into "inner part" of id container
+  cartModalBody.innerHTML = cartModalBodyContent;
+
 }
 
 
 
 /** CONTACT **/
+/** Get form with name of contact */
 let form = document.forms["contact"];
+
+/** When form submits (event of submitting), call the getValues function */
 form.addEventListener("submit", getValues);
 
 function getValues(event){
 
-    event.preventDefault();
+  // Stops the "normal" submitting process/event from happening
+  event.preventDefault();
 
-    let formData = {
-        "name": this.name.value
-    }
+  // Get value entered into input with name attribute
+  let formData = {
 
-    let toShow = `
+    "name": this.name.value
+
+  }
+
+  // Content that will be alerted to user
+  let toShow = `
     
-        Thank you for the message ${formData.name}
+    Thank you for the message ${formData.name}
     
-    `;
+  `;
 
-    alert(toShow);
-
+  // Alert content
+  alert(toShow);
 
 }
